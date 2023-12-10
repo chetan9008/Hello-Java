1d50d707c0e37d657f9e185491511a39ae947de8
chetan <chetankumar70174577@gmail.com>
2023-12-10 16:43:27 +0530

    completed java operator chapter

--- Chapter 3/Operators.java
+++ Chapter 3/Operators.java
@@ -45,5 +45,6 @@ public class Operators {
 
         System.out.println(chetan instanceof Student);
         
+        
     }
 }
---
+++ Chapter 3/typecasting.java
@@ -0,0 +1,20 @@
+import java.lang.*;
+
+public class typecasting {
+    public static void main(String[] args) {
+        long variable = 2;
+        System.out.println(variable);
+
+        float x = (float) 3 / 2;
+        System.out.println(x);
+
+        System.out.println();
+
+        double y = Math.cos(2.2);
+        y = Math.abs(2.2);
+        y = Math.sqrt(2.2);
+        y = Math.floor(2.9);
+        System.out.println(y);
+    }
+
+}
