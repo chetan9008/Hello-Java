ce24d5ef80174087f892f9b00101c7efdbdbdb03
chetan <chetankumar70174577@gmail.com>
2023-12-16 14:43:56 +0530

    fixing error in file

--- Chapter 2/symbolicConstant.java
+++ Chapter 2/symbolicConstant.java
@@ -1,35 +1,35 @@
-class Example {
-    void function(int x, double y) {
-        System.out.println("int : " + x + " float : " + y);
-    }
+// class Example {
+//     void function(int x, double y) {
+//         System.out.println("int : " + x + " float : " + y);
+//     }
 
-    void function(double y, int x) {
-        System.out.println("float : " + x + " int : " + y);
-    }
-}
+//     void function(double y, int x) {
+//         System.out.println("float : " + x + " int : " + y);
+//     }
+// }
 
 
 
-public class symbolicConstant {
-    public static void main(String[] args) {
-        final int VALUE = 2;
-        System.out.println(VALUE);
+// public class symbolicConstant {
+//     public static void main(String[] args) {
+//         final int VALUE = 2;
+//         System.out.println(VALUE);
 
-        int example_var1 = 89;
-        System.out.println((short) (example_var1));// data loss
-        System.out.println((long) (example_var1));
-        System.out.println((double) (example_var1));
-        System.out.println((char) (example_var1));
-        System.out.println((float) (example_var1));
-        System.out.println((int) (example_var1));
+//         int example_var1 = 89;
+//         System.out.println((short) (example_var1));// data loss
+//         System.out.println((long) (example_var1));
+//         System.out.println((double) (example_var1));
+//         System.out.println((char) (example_var1));
+//         System.out.println((float) (example_var1));
+//         System.out.println((int) (example_var1));
 
-        System.out.println("\n");
-        double example_var2 = 90.23;
-        System.out.println((int) example_var2);
-        System.out.println((char) example_var2);
-        System.out.println((double) example_var2);
+//         System.out.println("\n");
+//         double example_var2 = 90.23;
+//         System.out.println((int) example_var2);
+//         System.out.println((char) example_var2);
+//         System.out.println((double) example_var2);
 
-        Example ex = new Example();
-        ex.function(2.2,2);
-    }
-}
+//         Example ex = new Example();
+//         ex.function(2.2,2);
+//     }
+// }
