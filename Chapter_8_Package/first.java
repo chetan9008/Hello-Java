cd17e9c120891814ab2d76573872b69b768a33bb
chetan <chetankumar70174577@gmail.com>
2023-12-24 15:24:47 +0530

    package.java

---
+++ Chapter_8_Package/first.java
@@ -0,0 +1,13 @@
+package Chapter_8_Package;
+class Student {
+    String name = "chetan";
+    void display()
+    {
+        System.out.println("Name is " + name);
+    }
+}
+public class first {
+    public static void main(String[] args) {
+        System.out.println("hello java");
+    }
+}
---
+++ Chapter_8_Package/third.java
@@ -0,0 +1,8 @@
+package Chapter_8_Package;
+import Chapter_8_Package.second;
+public class third {
+    public static void main(String[] args) {
+        Teacher ex = new Teacher();
+        ex.show();
+    }
+}
