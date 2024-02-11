91ac1c0875b12294ae5a35b28d644974ba468ebb
chetan <chetankumar70174577@gmail.com>
2024-02-11 21:11:20 +0530

    practising for university exam

--- practise.java
+++ practise.java
@@ -1,25 +1,23 @@
-import java.util.Vector;
-
 /**
  * practise
  */
+import javax.swing.*;
+import java.awt.*;
 public class practise {
 
     public static void main(String[] args) {
-        int array[] = {3,4,5,9,2,5,6,};
-        for(int i=0;i<array.length;i++)
-        {
-            for (int j = i + 1; j < array.length; j++) {
-                if (array[i] > array[j]) {
-                    int temp = array[i];
-                    array[i] = array[j];
-                    array[j] = temp;
-                }
-            }
-        }
-        for(int i=0;i<array.length;i++)
-        {
-            System.out.println(array[i]);
-        }
+        JFrame frame = new JFrame();
+        frame.setSize(400,400);
+        frame.setLayout(new FlowLayout(FlowLayout.RIGHT));
+
+        JButton button1 = new JButton("1");
+        JButton button2 = new JButton("1");
+        JButton button3 = new JButton("1");
+
+        frame.add(button1);
+        frame.add(button2);
+        frame.add(button3);
+
+        frame.setVisible(true);
     }
 }
